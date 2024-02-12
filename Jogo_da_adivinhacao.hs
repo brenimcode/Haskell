e1ea9033db2dae6c4c3449c713d0000b657a2119
Breno Oliveira Cavalcante <127551374+brenimcode@users.noreply.github.com>
2024-02-12 15:38:53 -0300

    Update Jogo_da_adivinhacao.hs

--- Jogo_da_adivinhacao.hs
+++ Jogo_da_adivinhacao.hs
@@ -1,41 +1,55 @@
-module Main where
-
--- import System.Random (randomRIO)
-import Control.Monad (when)
+module Main (main) where
+ 
+import System.Random
 import System.IO (stdout,hSetBuffering,BufferMode(NoBuffering))
 
 mainLoop :: IO ()
-mainLoop = do 
-    hSetBuffering stdout NoBuffering
-    --numAleatorio <- randomRIO (1, 10) :: IO Int
-    putStrLn "Estou pensando em um número entre 1 e 10. Tente adivinhar.\n"
-    tentativa <- try 1 3
-
-    putStrLn "Digite 'S' para continuar ou qualquer outra coisa para parar:"
-    input <- getLine
-    if input == "s"
-      then mainLoop
-      else putStrLn "Fim..."
+mainLoop = do hSetBuffering stdout NoBuffering
+              numAleatorio <- randomRIO (1, 100) :: IO Int
+              putStrLn "Estou pensando em um número entre 1 e 100. Tente adivinhar.\n\n"
+              tentativa <- try 1 numAleatorio
+              recorde tentativa
+              putStr "Digite 'S' para continuar ou qualquer outra coisa para parar:"
+              input <- getLine
+              putStrLn ""
+              if (input == "S" || input == "s") then mainLoop
+                else  putStrLn "\nObrigado por jogar o Jogo de Adivinhação!\n"
 
 main :: IO () 
 main = do 
-    putStrLn "Bem vindo ao jogo da adivinhação\n"
-    mainLoop
--- Funcao para tentativa do numero --
+    putStrLn "\nBem-vindo ao Jogo da Adivinhação!\n"
+    mainLoop -- Inicia o loop
+
 try :: Int -> Int -> IO Int
 try a b = do
-    putStrLn ("Tentativa " ++ show a ++ ":")
-    putStr "Digite seu palpite: "
+    putStr "Tentativa " 
+    putStr (show a) 
+    putStrLn ":"
+    putStr "Digite seu palpite:"
     num <- getLine
-    if read num == b
-        then do
-            putStrLn "Seu palpite está correto!"
-            putStrLn  ("Parabéns! Você acertou em " ++ show a ++ " tentativas.")
-            return a
-        else if read num < b
-            then do
-                putStrLn ("Seu palpite de " ++ show num ++ " está abaixo do número correto.")
-                try (a + 1) b
-            else do
-                putStrLn ("Seu palpite de " ++ show num ++ " está acima do número correto.")
-                try (a + 1) b
+    if read num == b then do
+        putStrLn "Seu palpite está correto!"
+        putStr  "Parabéns! Você acertou em " 
+        putStr (show a) 
+        putStrLn " tentativas.\n\n"
+        return a
+    else if read num < b then do
+        putStr  "Seu palpite de " 
+        putStr (show num) 
+        putStrLn " está abaixo do número correto.\n\n"
+        try (a + 1) b
+    else do
+        putStr  "Seu palpite de " 
+        putStr (show num) 
+        putStrLn " está acima do número correto.\n\n"
+        try (a + 1) b
+
+recorde :: Int -> IO ()
+recorde a = do
+    conteudo <- readFile "highscore.txt"
+    let record = read conteudo :: Int 
+    if a < record then do
+        putStrLn "Você bateu o recorde!\n\n"
+        writeFile "highscore.txt" (show a)
+    else
+        return ()
